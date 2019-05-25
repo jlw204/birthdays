d23e9498b8190fd396d7cccd73f8f3f1a15db827
JLW <36314803+jlw204@users.noreply.github.com>
2019-05-25 13:58:23 -0700

    Update birthdays.py
    Refactored to remove need for global variables. General cleanup of variable names and docstrings.  Added namedtuple for greater flexibility if data needs to be output in other ways.

--- birthdays.py
+++ birthdays.py
@@ -1,8 +1,14 @@
-"""lb10Birthday.py - reformat exported birthday list for sacrament bulletin. 
+"""Birthdays.py - reformat exported birthday list for sacrament bulletin. 
                      Opens new file in Notepad to ease copy into Word.
      Parameters: filename (birthday list in raw format)
      Output: new_filename (same folder, same filename with new_ prepended)
     History:
+    2019-05-25 - refactored to remove global variables
+                cleanup() now returns namedtuple, includes month
+                list_month pulled from first entry in list
+                renamed variables
+                updated docstrings
+
     2019-05-09 - Refactored. 
                Can specify input file on command line or be prompted. 
                Some file error checking.
@@ -17,83 +23,80 @@
 import argparse
 import subprocess
 from pathlib import Path
+from collections import namedtuple
 
-# GLOBALS
-filename = None    # Path object
-list_month = None  # str ('May')
+# NAMED TUPLES
+Entry = namedtuple('Entry', "day month last first")
 
 
 def check_arguments():
     """Exit app with error if filename not specified or doesn't exist.
-       Modifies: filename (global, pathlib Path object)"""
-    global filename
-
+       Input: (str) full or partial path/filename from CLI
+       returns: (Path object) filename
+    """
     parser = argparse.ArgumentParser(
         description="Formats birthdays for church bulletin.")
-    parser.add_argument("filename", type=str, 
+    parser.add_argument("file", type=str, 
         help="birthday list exported from Ward PC")
     args = parser.parse_args()
 
-    filename = Path(args.filename)  # check file exists
+    filename = Path(args.file)
     if not filename.is_file():
         print(f"Error: File not found - {filename.absolute()}")
         exit(1)
+    return filename
 
 
-def get_birthday_list() -> list:
+def get_birthday_list(filename) -> list:
     """open and format raw birthday list as a list of raw entries
-    Input: None
-    Output: birthdays_raw (list): 'day month\nlast_name first_name'
-    globals used: filename
+       Input: (Path object) filename
+       returns: (list) birthdays_raw (['day month\nlast_name first_name',])
     """
     birthdays_raw = filename.read_text()
     return birthdays_raw.strip().split('\n\n\n')
 
 
-def cleanup(entry: str) -> str:
+def cleanup(entry: str):
     """Reformat a single birthday entry for the sacrament bulletin
-    Input: (str) entry - imported format (day month\nlast_name first_name)
-    Output: (str) entry - preferred export format (day - first_name last_name)
-    globals modified: list_month (str)
+       Input: (str) entry - ('day month\nlast_name first_name')
+       returns: (namedtuple) Entry(day, month, first, last)
     """
-    global list_month
-    
     bday, name = entry.split('\n')
     day, mon = bday.split(' ')
-    last, rest = name.split(', ')
+    last, first = name.split(', ')
     
-    if not list_month: 
-        list_month = mon  # Update month name for formatted file header
-
-    return f"{day} - {rest} {last}\n"
+    return Entry(day=day, month=mon, first=first, last=last)
 
 
-def save_formatted_birthday_list(birthdays: list) -> str:
-    """Save formatted birthday list with header, then launch
-    Input: None
-    Output: list - birthdays in format 'day month\nlast_name first_name'
-    globals used: filename, list_month
+def save_formatted_birthday_list(filename, birthdays: list) -> str:
+    """Save formatted birthday list, with header
+       Input: (Path) filename, (list) birthdays
+       returns: (str) filename of report
     """
-    output_file = str(filename.absolute().parent) + '\\' \
+    report_file = str(filename.absolute().parent) + '\\' \
                   + filename.stem + "_new" + filename.suffix
     
-    with open(output_file, 'w') as f:
+    with open(report_file, 'w') as f:
+        list_month = birthdays[0].month
         count = len(birthdays)
+
         f.write(f"{count} Birthdays for {list_month}\n"
                 f"Converted from {filename.absolute()}\n\n")
         for entry in birthdays:
-            f.write(entry)
-
-    print(f"Saved to {output_file}")
-    return output_file
+            f.write(f"{entry.day} - {entry.first} {entry.last}\n")
+    
+    print(f"Saved to {report_file}")
+    return report_file
 
 
 def main():
-    """Run program"""
-    check_arguments()
-    birthdays = [cleanup(entry) for entry in get_birthday_list()]
-    new_list = save_formatted_birthday_list(birthdays)
-    subprocess.run("notepad.exe " + new_list)
+    """Run program: generate formatted list and open in text editor
+    """
+    filename = check_arguments()
+    birthdays_raw = get_birthday_list(filename)
+    birthdays = [cleanup(entry) for entry in birthdays_raw]
+    bd_report = save_formatted_birthday_list(filename, birthdays)
+    subprocess.run("notepad.exe " + bd_report)
 
 
 if __name__ == "__main__":
